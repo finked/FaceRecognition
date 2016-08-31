65dbd1000681590cd2e2d4341cc508f8c0aed836
Tobias <tobias_maier99@gmx.de>
2016-08-31 23:37:52 +0200

    conv net: add learning rate/momentum updating and dropout

--- networks.py
+++ networks.py
@@ -1,6 +1,7 @@
 """ networks.py - the networks that are used for facial keypoint recognition """
 
 import numpy as np
+import theano
 
 from lasagne.updates import nesterov_momentum
 from lasagne import layers
@@ -68,27 +69,43 @@ class convolutionalNetwork:
                 ('input', layers.InputLayer),
                 ('conv1', layers.Conv2DLayer),
                 ('pool1', layers.MaxPool2DLayer),
+                ('dropout1', layers.DropoutLayer),
                 ('conv2', layers.Conv2DLayer),
                 ('pool2', layers.MaxPool2DLayer),
+                ('dropout2', layers.DropoutLayer),
                 ('conv3', layers.Conv2DLayer),
                 ('pool3', layers.MaxPool2DLayer),
+                ('dropout3', layers.DropoutLayer),
                 ('hidden4', layers.DenseLayer),
+                ('dropout4', layers.DropoutLayer),
                 ('hidden5', layers.DenseLayer),
                 ('output', layers.DenseLayer),
                 ],
             input_shape=(None, 1, 96, 96),
             conv1_num_filters=32, conv1_filter_size=(3, 3), pool1_pool_size=(2, 2),
+            dropout1_p=0.1,
             conv2_num_filters=64, conv2_filter_size=(2, 2), pool2_pool_size=(2, 2),
+            dropout2_p=0.2,
             conv3_num_filters=128, conv3_filter_size=(2, 2), pool3_pool_size=(2, 2),
-            hidden4_num_units=500, hidden5_num_units=500,
+            dropout3_p=0.3,
+            hidden4_num_units=500,
+            dropout4_p=0.5,
+            hidden5_num_units=500,
             output_num_units=30, output_nonlinearity=None,
 
-            update_learning_rate=0.01,
-            update_momentum=0.9,
+            # update_learning_rate=0.01,
+            # update_momentum=0.9,
 
             # data augmentation by flipping the images
             batch_iterator_train=augmentation.FlipBatchIterator(batch_size=128),
 
+            update_learning_rate=theano.shared(float32(0.03)),
+            update_momentum=theano.shared(float32(0.9)),
+
+            on_epoch_finished=[
+                AdjustVariable('update_learning_rate', start=0.03, stop=0.0001),
+                AdjustVariable('update_momentum', start=0.9, stop=0.999),
+                ],
             regression=True,
             max_epochs=epochs,
             verbose=1,
@@ -101,3 +118,27 @@ class convolutionalNetwork:
     def predict(self, X):
 
         return self.network.predict(X)
+
+
+
+def float32(k):
+    return np.cast['float32'](k)
+
+
+class AdjustVariable(object):
+    """
+    Helper function to adjust a variable inside a network after each epoch
+    """
+
+    def __init__(self, name, start=0.03, stop=0.001):
+        self.name = name
+        self.start, self.stop = start, stop
+        self.ls = None
+
+    def __call__(self, nn, train_history):
+        if self.ls is None:
+            self.ls = np.linspace(self.start, self.stop, nn.max_epochs)
+
+        epoch = train_history[-1]['epoch']
+        new_value = float32(self.ls[epoch - 1])
+        getattr(nn, self.name).set_value(new_value)
